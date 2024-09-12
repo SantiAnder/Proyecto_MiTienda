18898526b1b0a9820ca279f71bc73fcac58a59f1
Santiago <santiandermatten@gmail.com>
2024-09-11 23:21:20 -0300

    Begin Repository Pattern

---
+++ Orders/Orders_FrontEnd/Repositorios/HttpResponseWrapper.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+
+namespace Orders_FrontEnd.Repositorios
+{
+    public class HttpResponseWrapper<T>
+    {
+        public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpactionresponsemessage)
+        {
+            Response = response;
+            Error = error;
+            HttpResponseMessage = httpactionresponsemessage;
+        }
+
+        public T? Response { get; }
+        public bool Error { get; }
+        public HttpResponseMessage HttpResponseMessage { get; }
+
+        public async Task<string?> GetErrorMessageAsync()
+        {
+            if (!Error)
+            {
+                return null;
+            }
+            var statusCode = HttpResponseMessage.StatusCode;
+            if (statusCode == HttpStatusCode.NotFound)
+            {
+                return "Recurso no encontrado.";
+            }
+            if (statusCode == HttpStatusCode.BadRequest)
+            {
+                return await HttpResponseMessage.Content.ReadAsStringAsync();
+            }
+            if (statusCode == HttpStatusCode.Unauthorized)
+            {
+                return "Tienes que estar logeado para ejecutar la operación.";
+            }
+            if (statusCode == HttpStatusCode.Forbidden)
+            {
+                return "Tienes que estar logeado para ejecutar la operación.";
+            }
+
+            return "Ocurrió un error inesperado.";
+        }
+    }
+}
